a45357f0c742ec994120c95df0c870c487dbab87
James <yqingzh@gmail.com>
2025-04-14 17:15:54 +0800

    initial commit of website_1 crawler

---
+++ website_1/pipelines.py
@@ -0,0 +1,114 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from urllib.parse import urlparse
+from pathlib import Path
+import json
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class SaveJsonlPipeline:
+
+    # def open_spider(self, spider):
+    #     print(f"Starting crawling urls = '{spider.start_urls}'")
+
+    # def close_spider(self, spider):
+    #     print(f"Crawling finished.")
+
+    def process_item(self, item, spider):
+
+        try:
+            # create page folder
+            main_dir = Path('./data')
+            page_id = item['Source_page']
+            page_dir = main_dir / Path(f"page_{str(page_id)}")
+            index = item['Eia_report_index']
+            eia_report_index_dir = page_dir / Path(f"project_{str(index)}")
+            eia_report_index_dir.mkdir(parents=True, exist_ok=True)
+
+            # save metadata to json
+            file_path = eia_report_index_dir / "metadata.json"
+            with open (file_path, mode="w", encoding='utf-8') as f:
+                json.dump(item, f, ensure_ascii=False, indent=2)
+            spider.logger.info(f"Saved to {file_path}")
+        except Exception as e:
+            spider.logger.error(f"Failed to save JSON: {e}")
+
+        return item
+    
+class SaveHtmlPipeline:
+
+    @classmethod
+    def from_crawler(cls, crawler):
+
+        settings = crawler.settings
+        executable_path = settings.get("SELENIUM_DRIVER_EXECUTABLE_PATH")
+        driver_args = settings.getlist("SELENIUM_DRIVER_ARGUMENTS")
+        timeout = settings.getint("SELENIUM_PAGELOAD_TIMEOUT", 10)
+    
+        return cls(executable_path, driver_args, timeout)
+    
+    def __init__(self, executable_path, driver_args, timeout):
+        self.executable_path = executable_path
+        self.driver_args = driver_args
+        self.timeout = timeout
+        options = Options()
+        for arg in self.driver_args:
+            options.add_argument(arg)
+        self.driver = webdriver.Chrome(options=options)
+    
+    def close_spider(self, spider):
+        self.driver.quit()
+        
+    def process_item(self, item, spider):
+        detail_url = item['Detail_URL']
+        self.driver.get(detail_url)
+        wait = WebDriverWait(self.driver, self.timeout)
+        try:
+            wait.until(EC.presence_of_element_located((By.ID, "map")))
+            map_element = self.driver.find_element(By.ID, "map")
+            self.driver.execute_script("arguments[0].scrollIntoView();", map_element)
+            wait.until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, "path.leaflet-interactive")
+                )
+            )
+
+            # create page folder
+            main_dir = Path('./data')
+            page_id = item['Source_page']
+            page_dir = main_dir / Path(f"page_{str(page_id)}")
+            index = item['Eia_report_index']
+            eia_report_index_dir = page_dir / Path(f"project_{str(index)}")
+
+            image_name = "Raumbezug.jpg"
+            html_name = "Source.html"
+
+            image_path = eia_report_index_dir / image_name
+            html_path = eia_report_index_dir / html_name
+
+            map_element.screenshot(str(image_path))
+
+        except Exception as e:
+            spider.logger.error(f"Failed to save IMAGE: {e}")
+
+        try:
+            html = self.driver.page_source
+            f = open(html_path, mode="w", encoding="UTF-8")
+            f.write(html)
+            f.close()
+
+        except Exception as e:
+            spider.logger.error(f"Failed to save HTML: {e}")
+
+        return item
---
+++ website_1/spiders/website_1_spider.py
@@ -0,0 +1,386 @@
+from zipfile import ZipFile
+from pathlib import Path
+from io import BytesIO
+import scrapy
+import re
+
+
+class Website1Spider(scrapy.Spider):
+    name = "website_1"
+
+    custom_settings = {
+        # "DOWNLOAD_DELAY": 2,
+        # "CONCURRENT_REQUESTS": 1,
+        "RETRY_TIMES": 5,
+        "DEFAULT_REQUEST_HEADERS": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br, zstd"
+        },
+    }
+
+    start_urls = [
+        "https://www.uvp-verbund.de/freitextsuche?rstart=0&currentSelectorPage=1",
+    ]
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super().from_crawler(crawler, *args, **kwargs)
+        spider.page_limit = crawler.settings.getint("PAGE_LIMIT", default=5)
+        return spider
+
+    def parse(self, response):
+        
+        # get pagination links
+        buttons = response.css(".paging.caption > a.icon.small-button")
+        pagination_links = []
+        current_page_number = 1
+        for button in buttons:
+            button_name = button.css("a span::text").get()
+            button_href = button.css("a::attr(href)").get()
+            if button_name and button_href and button_name.isdigit() and 1 < int(button_name)<=self.page_limit:
+                pagination_links.append(button_href)
+            else:
+                if button_name and not button_href and button_name.isdigit() and 1 < int(button_name)<=self.page_limit:
+                    current_page_number = int(button_name)
+        print("Pagination links:", pagination_links)
+
+        yield from response.follow_all(pagination_links, self.parse)
+
+        # get eia report links
+        eia_report_links = list(dict.fromkeys(response.css(".data > .teaser-data.search > a::attr(href)").getall()))
+
+        # cleaned_eia_links = []
+        # for link in eia_report_links:
+        #     if "?" in link and ";" in link:
+        #         cleaned_link = f"{link.split(';')[0]}?{link.split('?')[1]}"
+        #         cleaned_eia_links.append(cleaned_link) 
+
+        for i, link in enumerate(eia_report_links, start=1):
+            eia_report_index = (current_page_number - 1)*len(eia_report_links) + i
+            meta = response.meta.copy()
+            meta["eia_report_index"] = eia_report_index
+            meta["source_page"] = current_page_number
+            yield response.follow(link, callback=self.parse_metadata, meta=meta)     
+        # yield from response.follow_all(cleaned_eia_links, self.parse_metadata)   
+                                              
+    def parse_metadata(self, response):
+
+        # get the metadata of the page:
+
+        eia_report_index = response.meta["eia_report_index"]
+        source_page = response.meta["source_page"]
+        report_type = response.xpath('//div[@class="helper text"]/span/text()').get()
+        if report_type:
+            report_type = report_type.strip()
+
+        # self.logger.debug(f"Page {source_page} (index {eia_report_index}) - report_type = {report_type}")
+
+
+        if report_type == "Negative Vorprüfungen":
+            yield from self.parse_negative_vorpruefung(response, eia_report_index, source_page, report_type)
+
+        elif report_type == "Zulassungsverfahren":
+            yield from self.parse_zulassungsverfahren(response, eia_report_index, source_page, report_type)
+
+    def parse_negative_vorpruefung(self, response, eia_report_index, source_page, report_type):
+
+        # "Negative Vorprüfungen"
+        # if report_type == "Negative Vorprüfungen":
+        title = response.xpath('//div[@class="columns"]/h1/text()').get()
+        if title:
+            title = title.strip()
+        last_updated = response.xpath('//div[@class="helper text date"]/span/text()').get()
+        last_updated_title = "Zuletzt geändert"
+        if last_updated:
+            last_updated_title = f"{last_updated.split(' ')[0]} {last_updated.split(' ')[1]}"
+            last_updated = last_updated.split(" ")[2]
+        project_description_title = response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()').get()
+        if project_description_title:
+            project_description_title = project_description_title.strip()
+        project_description = response.xpath('//div[@class="columns"]/p/text()').get()
+        if project_description:
+            project_description = project_description.strip()
+        
+        if response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()')[1].get():
+            uvp_category_title = response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()')[1].get()
+        else:
+            uvp_category_title = "UVP-Kategorie"
+        uvp_category = response.xpath('//div[@class="list-item"]/div/span[@class="text"]/text()').getall()
+        adressen_title = "Adressen"
+        if response.xpath('//div[@class="columns form"]/h3/text()').get():
+            adressen_title = response.xpath('//div[@class="columns form"]/h3/text()').get()
+        ansprechpartner_title = "Ansprechpartner"
+        if response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()').get():
+            ansprechpartner_title = response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()').get()
+        location = response.xpath('//div[@class="columns form"]/p/text()').getall()
+        cleaned_location = []
+        if location:
+            for value in location:
+                value = value.strip()
+                value = re.sub(r'\s+', ' ', value).strip()
+                cleaned_location.append(value)
+        email = response.xpath('//td/a/@href').get()
+        if email:
+            if isinstance(email, str):
+                if ":" in email:
+                    email = email.split(":")[1].strip()
+        
+        phone = response.xpath('//tr[td[contains(text(), "Telefon")]]/td[2]/text()').get()
+        if phone:
+            phone = phone.strip()
+        website = ""
+        if len(response.xpath('//td/a/@href').getall()) > 1:
+            website = response.xpath('//td/a/@href').getall()[1]
+        decision_date_title = "Datum der Entscheidung"
+        if response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()')[1].get():
+            decision_date_title = response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()')[1].get().strip()
+        decision_date = cleaned_location[-1]
+        attachment_title = response.xpath('//div[@class="columns form"]/h4[@class="title-font"]/text()').get()
+        if attachment_title:
+            attachment_title = attachment_title.strip()
+        eia_report_pdf_title = response.xpath('//a[@class="link download"]/@title').getall()
+        eia_report_pdf_url = response.xpath('//a[@class="link download"]/@href').getall()
+        zip_url = response.xpath('//div[@class="zip-download"]/a/@href').get()
+        metadata = {
+            "Detail_URL": response.url,
+            "Eia_report_index" : eia_report_index,
+            "Source_page" : source_page,
+            "report_type" : report_type,
+            "Title" : title,
+            last_updated_title: last_updated,
+            project_description_title: project_description,
+            uvp_category_title: uvp_category,
+            adressen_title:{
+                ansprechpartner_title:{
+                    "location": cleaned_location[:-1],
+                    "E-Mail": email,
+                    "Telefon": phone,
+                    "URL": website,
+                },
+            },
+            decision_date_title: decision_date,
+            attachment_title: {
+                "Title": eia_report_pdf_title,
+                "URL": eia_report_pdf_url,
+                "ZIP_URL": zip_url,
+            },
+        }
+        yield metadata
+
+        # scrapy.Request() to download zipfile
+        if zip_url:
+            yield scrapy.Request(
+                url=zip_url,
+                callback=self.save_zip,
+                meta={
+                    "eia_report_index": eia_report_index,
+                    "source_page": source_page,
+                },
+            
+            )
+
+    def parse_zulassungsverfahren(self, response, eia_report_index, source_page, report_type):
+
+        # "Zulassungsverfahren"
+        # if report_type == "Zulassungsverfahren":
+        title = response.xpath('//div[@class="columns"]/h1/text()').get()
+        if title:
+            title = title.strip()
+        last_updated = response.xpath('//div[@class="helper text date"]/span/text()').get()
+        last_updated_title = "Zuletzt geändert"
+        if last_updated:
+            last_updated_title = f"{last_updated.split(' ')[0]} {last_updated.split(' ')[1]}"
+            last_updated = last_updated.split(" ")[2]
+        project_description_title = response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()').get()
+        if project_description_title:
+            project_description_title = project_description_title.strip()
+        project_description = response.xpath('//div[@class="columns"]/p/text()').get()
+        if project_description:
+            project_description = project_description.strip()
+        
+        if response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()')[1].get():
+            uvp_category_title = response.xpath('//div[@class="columns"]/h3[@class="title-font"]/text()')[1].get()
+        else:
+            uvp_category_title = "UVP-Kategorie"
+        uvp_category = response.xpath('//div[@class="list-item"]/div/span[@class="text"]/text()').getall()
+        adressen_title = "Adressen"
+        if response.xpath('//div[@class="columns form"]/h3/text()').get():
+            adressen_title = response.xpath('//div[@class="columns form"]/h3/text()').get()
+        ansprechpartner_title = "Ansprechpartner"
+        if response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()').get():
+            ansprechpartner_title = response.xpath('//div[@class="columns form"]/h4[@class="no-margin"]/text()').get()
+        location = response.xpath('//div[@class="columns form"]/p/text()').getall()
+        cleaned_location = []
+        if location:
+            for value in location:
+                value = value.strip()
+                value = re.sub(r'\s+', ' ', value).strip()
+                cleaned_location.append(value)
+        email = response.xpath('//td/a/@href').get()
+        if email:
+            if isinstance(email, str):
+                if ":" in email:
+                    email = email.split(":")[1].strip()
+        phone = response.xpath('//tr[td[contains(text(), "Telefon")]]/td[2]/text()').get()
+        if phone:
+            phone = phone.strip()
+        website = ""
+        if len(response.xpath('//td/a/@href').getall()) > 1:
+            website = response.xpath('//td/a/@href').getall()[1]
+        metadata = {
+            "Detail_URL": response.url,
+            "Eia_report_index" : eia_report_index,
+            "Source_page" : source_page,
+            "report_type" : report_type,
+            "Title" : title,
+            last_updated_title: last_updated,
+            project_description_title: project_description,
+            uvp_category_title: uvp_category,
+            adressen_title:{
+                ansprechpartner_title:{
+                    "location": cleaned_location,
+                    "E-Mail": email,
+                    "Telefon": phone,
+                    "URL": website,
+                },
+            },
+        }
+        attachment_title = response.xpath('//div[@id="timeline"]/div[@class="columns"]/h1/text()').get()
+        if attachment_title:
+            attachment_title = attachment_title.strip()
+        attachment_metadata = Website1Spider.attachment_metadata_zulassungsverfahren(response)
+        metadata[attachment_title] = attachment_metadata[attachment_title]
+        yield metadata
+
+        # # scrapy.Request() to download zipfile
+        # zip_url =  metadata[report_type]["ZIP-URL"]
+        # if zip_url:
+        #     yield scrapy.Request(
+        #         url=zip_url,
+        #         callback=self.save_zip,
+        #         meta={
+        #             "eia_report_index": eia_report_index,
+        #             "source_page": source_page,
+        #         },
+            
+        #     )
+
+    def save_zip(self, response):
+
+        eia_report_index = response.meta["eia_report_index"]
+        page_id = response.meta["source_page"]
+
+        dir_path = Path("./data") / f"page_{page_id}" / f"project_{eia_report_index}"
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        try:
+            with ZipFile(BytesIO(response.body)) as z:
+                z.extractall(path=dir_path / "attachment")
+            self.logger.info(f"Extracted ZIP to {dir_path / 'attachment'}")
+        except Exception as e:
+            self.logger.error(f"Failed to unzip: {e}")
+
+    @staticmethod
+    def attachment_metadata_zulassungsverfahren(response):
+        timeline_text = response.xpath('//div[@class="timeline-text"]')
+        attachment_title = response.xpath('//div[@id="timeline"]/div[@class="columns"]/h1/text()').get()
+        zip_link_title = "ZIP-URL"# response.xpath('.//div[@class="zip-download"]/a/@title').get()
+        zip_url = response.xpath('.//div[@class="zip-download"]/a/@href').get()
+        if attachment_title:
+            attachment_title = attachment_title.strip()
+        attachment_metadata = {}
+        # icon-dot
+        attachment_metadata[attachment_title] = {}
+        attachment_metadata[attachment_title][zip_link_title] = zip_url
+
+        if timeline_text:
+            icon_dots = timeline_text.xpath('.//h2[@class="icon-dot" or @class="icon-check"]')
+            if icon_dots:
+                icon_count = 1
+                num_icon_dot = len(icon_dots)
+                
+                for dot in icon_dots:
+
+                    siblings = dot.xpath('following-sibling::*')
+                    local_scope = []
+                    for sib in siblings:
+                        cls = sib.attrib.get("class", "")
+                        tag = sib.root.tag
+                        # if tag == "h4" and ("title-font" in cls):
+                        if tag == "h2" and ("icon-dot" in cls or "icon-check" in cls):
+                            break
+                        local_scope.append(sib)
+                    
+                    dot_title = dot.xpath('text()').get()
+
+                    if dot_title:
+                        dot_title = dot_title.strip()
+                    print(f"dot_title: {dot_title}")
+
+                    # no-margin
+                    decision_date_title = dot.xpath('following-sibling::h4[@class="no-margin"][1]/text()').get()
+                    if decision_date_title:
+                        decision_date_title = decision_date_title.strip()
+                    print(f"decision_date_title: {decision_date_title}")
+
+                    decision_date = dot.xpath('following-sibling::p[1]/text()').get()
+                    if decision_date:
+                        decision_date = decision_date.strip()
+                    if "-" in decision_date:
+                        decision_date = f"{decision_date.split('-')[0].strip()} - {decision_date.split('-')[1].strip()}"
+                    print(f"decision_date: {decision_date}")
+
+                    # title-font
+                    file_group_title = [node for node in local_scope if node.root.tag == "h4" and "title-font" in node.attrib.get("class", "")]
+
+                    attachment_metadata[attachment_title][f"{str(icon_count)}-{dot_title}"] = {}
+                    attachment_metadata[attachment_title][f"{str(icon_count)}-{dot_title}"][decision_date_title] = decision_date
+                    if file_group_title:
+
+                        num_file_group = len(file_group_title)
+                        for group in file_group_title:
+                            
+                            siblings = group.xpath('following-sibling::*')
+                            title_scope = []
+                            for sib in siblings:
+                                cls = sib.attrib.get("class", "")
+                                tag = sib.root.tag
+                                if tag == "h4" and ("title-font" in cls):
+                                # if tag == "h2" and ("icon-dot" in cls or "icon-check" in cls):
+                                    break
+                                title_scope.append(sib)
+
+                            file_group_title_text = group.xpath('text()').get()
+                            if file_group_title_text:
+                                file_group_title_text = file_group_title_text.strip()
+                            print(f"file_group_title_text: {file_group_title_text}")
+                            
+                            document_lists = [node for node in title_scope if node.root.tag == "div" and "document-list" in node.attrib.get("class", "")]
+                            docs_metadata = {}
+                            list_id = 1
+                            if document_lists:
+                                for dl in document_lists:
+                                    documents = dl.xpath('div[@class="list-item"]')
+                                    if documents:
+                                        num_documents = len(documents)
+                                        for doc in documents:
+                                            doc_metadata = {}
+                                            doc_url = doc.xpath('a[@class="link download"]/@href').get()
+                                            if doc_url:
+                                                doc_url = doc_url.strip()
+                                            doc_title = doc.xpath('a[@class="link download"]/@title').get()
+                                            if doc_title:
+                                                doc_title = doc_title.strip()
+                                            print(f"doc_title: {doc_title}")
+                                            print(f"doc_url: {doc_url}")
+                                            doc_metadata = {
+                                                'Document-title': doc_title,
+                                                'Document-URL': doc_url,
+                                            }
+                                            docs_metadata[str(list_id)] = doc_metadata
+                                            list_id += 1
+                            attachment_metadata[attachment_title][f"{str(icon_count)}-{dot_title}"][file_group_title_text] = docs_metadata
+                    icon_count += 1
+        return attachment_metadata 
